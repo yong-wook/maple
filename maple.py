6622f09eb5ff58c3515627631012eee870ead2ab
yong-wook <91772701+yong-wook@users.noreply.github.com>
2023-12-18 18:21:27 +0900

    Add files via upload

--- maple.py
+++ maple.py
@@ -4,7 +4,6 @@ import streamlit as st
 import datetime
 
 
-
 def get_cube_history(api_key, date):
     date_str = date.strftime("%Y%m%d")
     header = {'x-nxopen-api-key': api_key}
@@ -14,69 +13,71 @@ def get_cube_history(api_key, date):
     if "error" in response:
         st.header(response["error"]["message"])
         return None
-    elif len(response["cube_history"]) !=0 :
+    elif len(response["cube_history"]) != 0:
         return response["cube_history"]
-    elif len(response["cube_history"]) ==0:
+    elif len(response["cube_history"]) == 0:
         st.header("큐브 사용 내역이 없습니다.")
-        return  None
+        return None
 
-def display_cube_information(cube_history):
-    if not cube_history:
-        return
 
-    count_cube = len(cube_history)
-   
-    if count_cube == 1000:
-        count_cube = "1000번 넘도록"
-    else:
-        count_cube = f"{count_cube}번"
-    st.write(f"당신은 이날 {count_cube} 큐브질을 했습니다.")
-    df = pd.DataFrame(cube_history)
+def filter_by_character(df, selected_characters):
+    if not selected_characters:
+        return df
+    return df[df["character_name"].isin(selected_characters)]
 
+
+def display_cube_results(df):
     st.write("큐브 등업 결과")
     df0 = df["item_upgrade_result"].value_counts().reset_index()
     df0.columns = ["item_upgrade_result", "count"]
-        
-    df0_style = df0.style.apply(lambda x: ['background-color: tan' if x["item_upgrade_result"] == '성공' else '' for _ in df0.columns], axis=1)
+
+    df0_style = df0.style.apply(lambda x: ['background-color: tan' if x["item_upgrade_result"] == '성공' else '' for _ in df0.columns],
+                                axis=1)
     st.dataframe(df0_style, use_container_width=True)
 
-    
 
-    df1 = df[["character_name", "world_name", "cube_type", "target_item", "date_create"]]
-    
-    st.dataframe(df1, use_container_width=True)       
+def display_character_info(df):
+    st.markdown(f"# **사용 캐릭터:** {', '.join(df['character_name'].unique())}")
+    selected_characters = []
 
-    #dl = []
-    #potential_options = [entry['value'] for entry in df['after_potential_option']]
-    #dl.append(potential_options)
-    #for i in range(len(df)):
-        #dic = df["after_potential_option"][i]
-        #dls = []
-        #for j in range(len(dic)):
-        #    dv = dic[j]
-        #    dv = dv["value"]
-        #    dls.append(dv)
-        #dl.append(dls)
-    #df2 = pd.DataFrame(dl)
+    # 체크박스로 바꿔보기
+    st.markdown("### **사용 캐릭터 필터링**")
+    for character in df['character_name'].unique():
+        selected = st.checkbox(character)
+        if selected:
+            selected_characters.append(character)
+
+    if not selected_characters:
+        st.warning("하나 이상의 캐릭터를 선택하세요.")
+
+    cube_history_filtered = filter_by_character(df, selected_characters)
     
-    df2 = pd.DataFrame([
-    [entry["value"] for entry in cube]
-    for cube in df["after_potential_option"]
-    ])
+    df1 = cube_history_filtered[["character_name", "world_name", "cube_type", "target_item", "date_create"]]
+    st.dataframe(df1, use_container_width=True)
+    return cube_history_filtered  # 선택된 캐릭터에 대한 데이터프레임 반환
+
+
+def display_potential_options(selected_characters_df):
+    #print(selected_characters_df[["character_name", "world_name", "cube_type", "target_item", "date_create","potential_option_grade"]])
+    max_num_options = 3  # 잠재 옵션을 3개로 맞춥니다.
+    df2 = pd.DataFrame([[entry["value"] for entry in cube] + [None] * (max_num_options - len(cube)) for cube in selected_characters_df["after_potential_option"]])
+    df2.columns = [f"{i + 1}번째" for i in range(max_num_options)]
+    df2["등급"] = ""
     
-    df2.columns = ["첫번째", "두번째", "세번째"]
-    df2["등급"] = df["potential_option_grade"]
+    abc = selected_characters_df["potential_option_grade"].tolist()
+    for i in range(len(df2)):
+        df2["등급"][i] = abc[i]
     
     grade_colors = {
-    '레전드리': 'background-color: green',
-    '유니크': 'background-color: yellow',
-    '에픽': 'background-color: purple',
-    '레어': 'background-color: blue'
+        '레전드리': 'background-color: green',
+        '유니크': 'background-color: yellow',
+        '에픽': 'background-color: purple',
+        '레어': 'background-color: blue',
     }
     df2_style = df2.style.apply(lambda x: [grade_colors[x["등급"]] for _ in df2.columns], axis=1)
-    
 
     st.dataframe(df2_style, use_container_width=True)
+
 
 def main():
     st.set_page_config(
@@ -93,10 +94,12 @@ def main():
     else:
         date = st.date_input("확인하고 싶은 날짜를 선택하세요.", datetime.datetime.today(), format="YYYY/MM/DD")
         cube_history = get_cube_history(key, date)
-        
+
         if cube_history:
-            display_cube_information(cube_history)
-                  
+            df = pd.DataFrame(cube_history)
+            display_cube_results(df)
+            selected_characters_df = display_character_info(df)
+            display_potential_options(selected_characters_df)
 
 
 if __name__ == "__main__":
