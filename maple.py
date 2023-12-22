78e673c4161e95e20da3fad2bdaff6b0df1d4d1e
yong-wook <91772701+yong-wook@users.noreply.github.com>
2023-12-22 20:21:51 +0900

    Add files via upload

--- maple.py
+++ maple.py
@@ -3,81 +3,8 @@ import pandas as pd
 import streamlit as st
 import datetime
 
-key = "live_50138df357699939f3b790093592e8e075fc0519e008653d27fc175e2dc9da5ad961e05fef3f2d19e258e30bf46403b"
-
-def get_cube_history(api_key, date):
-    date_str = date.strftime("%Y%m%d")
-    header = {'x-nxopen-api-key': api_key}
-    url = f"https://open.api.nexon.com/maplestory/v1/history/cube?count=1000&date_kst={date_str}"
-    response = requests.get(url, headers=header).json()
-
-    if "error" in response:
-        st.header(response["error"]["message"])
-        return None
-    elif len(response["cube_history"]) != 0:
-        return response["cube_history"]
-    elif len(response["cube_history"]) == 0:
-        st.header("큐브 사용 내역이 없습니다.")
-        return None
 
 
-def filter_by_character(df, selected_characters):
-    if not selected_characters:
-        return df
-    return df[df["character_name"].isin(selected_characters)]
-
-
-def display_cube_results(df):
-    st.write("큐브 등업 결과")
-    df0 = df["item_upgrade_result"].value_counts().reset_index()
-    df0.columns = ["item_upgrade_result", "count"]
-
-    df0_style = df0.style.apply(lambda x: ['background-color: tan' if x["item_upgrade_result"] == '성공' else '' for _ in df0.columns],
-                                axis=1)
-    st.dataframe(df0_style, use_container_width=True)
-
-
-def display_character_info(df):
-    st.markdown(f"# **사용 캐릭터:** {', '.join(df['character_name'].unique())}")
-    selected_characters = []
-
-    # 체크박스로 바꿔보기
-    st.markdown("### **사용 캐릭터 필터링**")
-    for character in df['character_name'].unique():
-        selected = st.checkbox(character)
-        if selected:
-            selected_characters.append(character)
-
-    if not selected_characters:
-        st.warning("하나 이상의 캐릭터를 선택하세요.")
-
-    cube_history_filtered = filter_by_character(df, selected_characters)
-    
-    df1 = cube_history_filtered[["character_name", "cube_type", "target_item", "date_create"]]
-    st.dataframe(df1, use_container_width=True)
-    return cube_history_filtered  # 선택된 캐릭터에 대한 데이터프레임 반환
-
-
-def display_potential_options(selected_characters_df):
-    #print(selected_characters_df[["character_name", "world_name", "cube_type", "target_item", "date_create","potential_option_grade"]])
-    max_num_options = 3  # 잠재 옵션을 3개로 맞춥니다.
-    df2 = pd.DataFrame([[entry["value"] for entry in cube] + [None] * (max_num_options - len(cube)) for cube in selected_characters_df["after_potential_option"]])
-    df2.columns = [f"{i + 1}번째" for i in range(max_num_options)]
-    df2["등급"] = ""
-    
-    abc = selected_characters_df["potential_option_grade"].tolist()
-    for i in range(len(df2)):
-        df2["등급"][i] = abc[i]
-    
-    grade_colors = {
-        '레전드리': 'background-color: green',
-        '유니크': 'background-color: yellow',
-        '에픽': 'background-color: purple',
-        '레어': 'background-color: blue',
-    }
-    df2_style = df2.style.apply(lambda x: [grade_colors[x["등급"]] for _ in df2.columns], axis=1)
-
-    st.dataframe(df2_style, use_container_width=True)
 
 
 def main():
@@ -86,22 +13,13 @@ def main():
         page_icon="👍"
     )
 
-    st.header("일단 openapi.nexon.com에 가서 Api 키를 발급 받아야 합니다.")
+    st.header("각종 메이플 정보 검색 공간입니다.")
 
-    key = st.text_input('api키 입력', '', type="password")
-
-    if key == '':
-        st.header("api키가 없으면 조회가 안됩니다.")
-    else:
-        date = st.date_input("확인하고 싶은 날짜를 선택하세요.", datetime.datetime.today(), format="YYYY/MM/DD")
-        cube_history = get_cube_history(key, date)
-
-        if cube_history:
-            df = pd.DataFrame(cube_history)
-            display_cube_results(df)
-            selected_characters_df = display_character_info(df)
-            display_potential_options(selected_characters_df)
+                  
 
 
 if __name__ == "__main__":
     main()
+
+
+
---
+++ pages/캐릭터정보.py
@@ -0,0 +1,48 @@
+import requests
+import pandas as pd
+import streamlit as st
+import datetime
+
+api_key = "live_50138df357699939f3b790093592e8e075fc0519e008653d27fc175e2dc9da5ad961e05fef3f2d19e258e30bf46403b6"
+header = {'x-nxopen-api-key': api_key}
+
+character_name = st.text_input('캐릭터명을 입력하세요', '아델',)
+#ocid 발급
+url = f"https://open.api.nexon.com/maplestory/v1/id?character_name={character_name}"
+res = requests.get(url, headers= header).json()
+if "ocid" in res.keys():
+    ocid = res["ocid"]
+    
+    #기본정보 조회
+    s_date = datetime.datetime.today() - datetime.timedelta(days=1)
+    s_date = s_date.strftime("%Y-%m-%d")
+    url = f"https://open.api.nexon.com/maplestory/v1/character/basic?ocid={ocid}&date={s_date}"
+    res = requests.get(url, headers= header).json()
+
+    df = pd.DataFrame.from_dict([res])
+    df1 = df[["character_name", "world_name", "character_gender", "character_class", "character_class_level", "character_guild_name"]] 
+    df1 = df1.rename(columns={"character_name":"캐릭명", "world_name":"서버", "character_gender":"성별", "character_class":"직업", "character_class_level":"달성전직", "character_guild_name":"길드명"})
+
+
+    c_img = df["character_image"][0]
+    st.markdown(f'<img src="{c_img}">', unsafe_allow_html= True)
+    st.dataframe(df1)
+
+    # 무릉도장 정보 조회
+    url = f"https://open.api.nexon.com/maplestory/v1/character/dojang?ocid={ocid}&date={s_date}"
+    res = requests.get(url, headers= header).json()
+    best_record = res["dojang_best_floor"]
+    best_date = res["date_dojang_record"]
+    best_time = res["dojang_best_time"]
+
+    st.header(f"무릉도장 최고층수: {best_record}")
+    st.header(f"무릉도장 달성일: {best_date[:10]}")
+    st.header(f"무릉도장 소요시간(초): {best_time}")
+
+
+else:
+    st.header(f"{character_name}: 해당 캐릭터의 기록이 없어요")
+
+
+
+    
