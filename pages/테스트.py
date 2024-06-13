d9aaf331f0b85835ee4728bdc007d22b9b06b563
yong-wook <91772701+yong-wook@users.noreply.github.com>
2024-06-13 12:08:08 +0900

    Update 테스트.py

--- pages/테스트.py
+++ pages/테스트.py
@@ -3,6 +3,6 @@ import streamlit as st
 #url = "https://www.naver.com"
 #res = requests.get(url, verify=False)
 
-st.markdown("<iframe
+st.markdown('''<iframe
             scr = "https://www.naver.com"
-></iframe>", unsafe_allow_html=True)
+></iframe>''', unsafe_allow_html=True)
